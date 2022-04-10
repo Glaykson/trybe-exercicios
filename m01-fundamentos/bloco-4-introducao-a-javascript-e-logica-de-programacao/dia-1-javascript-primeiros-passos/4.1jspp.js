3159c9393b43f3fc4646d50a1ab05d58caa98821
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-10 20:07:14 -0300

    adiciona exercício 2 a 4.1jspp.js

--- m01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4.1jspp.js
+++ m01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4.1jspp.js
@@ -1,8 +1,22 @@
-const a = 10;
-const b = 5;
+// exercícios - 01
+
+const a = 20;
+const b = 10;
 
 console.log("soma: " + (a + b));
 console.log("subtração: " + (a - b));
 console.log("multiplicação: " + (a * b));
 console.log("divisão: " + (a / b));
 console.log("módulo: " + (a % b));
+
+
+// exercícios - 02
+
+const x = 5;
+const y = 15;
+
+if (x > y) {
+    console.log("'x' é maior que 'y'");
+} else {
+    console.log("'y' é maior que 'x'");
+}
