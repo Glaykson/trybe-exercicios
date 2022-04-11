c56f01f86e8cfb3c862ca71b43e4477777c39991
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-11 15:56:18 -0300

    adiciona exercício 7 a 4.1jspp.js

--- m01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4.1jspp.js
+++ m01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4.1jspp.js
@@ -90,3 +90,25 @@ switch (pecaDoXadrez.toLowerCase()) {
     break;
   default: console.log('Erro: peça inválida')
 }
+
+
+// exercício - 07
+
+const nota = 45;
+
+if (nota < 0 || nota > 100) {
+  console.log('Erro: nota inesistente');
+} else if (nota >= 90) {
+  console.log("A");
+} else if (nota >= 80) {
+  console.log("B");
+} else if (nota >= 70) {
+  console.log("C");
+} else if (nota >= 60) {
+  console.log("D");
+} else if (nota >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
+}
+
