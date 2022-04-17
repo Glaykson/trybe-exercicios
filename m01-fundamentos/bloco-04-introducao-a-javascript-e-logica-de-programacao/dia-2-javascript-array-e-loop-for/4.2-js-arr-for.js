943e61d7e19e3060a6d96e203d3ffbdddaf7db5b
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-17 19:20:40 -0300

    exercicios/4.2 - 6

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/4.2-js-arr-for.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/4.2-js-arr-for.js
@@ -45,7 +45,7 @@ for (let index = 0; index < numbers.length; index += 1) {
 
 let media = soma / numbers.length;
 
-if (media > 20) {
+  if (media > 20) {
     console.log('valor maior que 20');
   } else {
     console.log('valor menor ou igual a 20');
@@ -69,6 +69,19 @@ console.log(maiorNum)
 // Exercício 6 - Descubra quantos valores ímpares existem no array e imprima o soma. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";  
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    soma += 1;      
+  }
+}
+
+if (soma === 0) {
+  console.log("nenhum valor ímpar encontrado")
+} else {
+  console.log(soma)  
+}
 
 
 // Exercício 7 - Utilizando for , descubra qual o menor valor contido no array e imprima-o; 
