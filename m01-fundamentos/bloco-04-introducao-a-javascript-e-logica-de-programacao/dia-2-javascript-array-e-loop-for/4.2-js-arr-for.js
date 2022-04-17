3170dececccfdc8c27370598fc3d7d25ca42e6a4
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-17 17:54:52 -0300

    exercicios/4.2 - 1

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/4.2-js-arr-for.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/4.2-js-arr-for.js
@@ -1,4 +1,54 @@
-// 4.2 - JavaScript - Array e loop For
+//      4.2 - JavaScript - Array e loop For
+
+// Exercício 1 - Percorra o array imprimindo todos os valores nele contidos com a função console.log(); 
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index+= 1) {
+  console.log(numbers[index]);
+}  
+
+
+// Exercício 2 - Some todos os valores contidos no array e imprima o resultado;  
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+// Exercício 3 - Calcule e imprima a média aritmética dos valores contidos no array;  
+// A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos. 
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+// Exercício 4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";  
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+// Exercício 5 - Utilizando for , descubra qual o maior valor contido no array e imprima-o; 
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+// Exercício 6 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";  
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+// Exercício 7 - Utilizando for , descubra qual o menor valor contido no array e imprima-o; 
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+// Exercício 8 - Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+// Exercício 9 - Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 . 
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 
 
 
