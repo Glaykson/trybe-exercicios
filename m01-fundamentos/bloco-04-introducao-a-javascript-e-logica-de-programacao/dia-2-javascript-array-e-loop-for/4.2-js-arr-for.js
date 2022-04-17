98a04b020e7ab92f8ee3d3ba717375c22a77dc93
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-17 19:06:02 -0300

    exercicios/4.2 - 5

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/4.2-js-arr-for.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/4.2-js-arr-for.js
@@ -56,6 +56,14 @@ if (media > 20) {
 // Exercício 5 - Utilizando for , descubra qual o maior valor contido no array e imprima-o; 
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNum = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] > maiorNum) {
+    maiorNum = numbers[index];
+  }
+}
+console.log(maiorNum)
 
 
 // Exercício 6 - Descubra quantos valores ímpares existem no array e imprima o soma. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";  
