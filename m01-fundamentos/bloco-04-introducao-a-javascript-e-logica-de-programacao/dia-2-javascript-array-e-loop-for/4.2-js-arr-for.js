b723808d7c9be9fdea088179498b896f259bf745
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-17 19:41:45 -0300

    exercicios/4.2 - 9

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/4.2-js-arr-for.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/4.2-js-arr-for.js
@@ -117,5 +117,13 @@ console.log(numeros)
 
 // Exercício 9 - Utilizando o array criado no exercício anterior imprima o soma da divisão de cada um dos elementos por 2 . 
 
+let numeros = [];
 
+for (let index = 1; index <= 25; index += 1) {
+    numeros.push(index);
+}
+
+for (let index = 0; index < numeros.length; index += 1) {
+    console.log(numeros[index] / 2);
+  };
 
