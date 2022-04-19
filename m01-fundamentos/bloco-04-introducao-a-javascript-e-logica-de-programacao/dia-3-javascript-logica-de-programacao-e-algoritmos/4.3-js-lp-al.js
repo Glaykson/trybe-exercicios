3dca0e179d7412504542cf051899bbe4a5c6ce09
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-19 10:43:30 -0300

    exercicios/4.3 - 1

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/4.3-js-lp-al.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/4.3-js-lp-al.js
@@ -5,6 +5,13 @@
 // 4! = 4 x 3 x 2 x 1 = 24
 // Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
 
+let fatorial = 1;
+
+for (index = 10; index > 0; index -= 1) {
+  fatorial *= index;  
+}
+  console.log(fatorial);
+
 
 
 // 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
