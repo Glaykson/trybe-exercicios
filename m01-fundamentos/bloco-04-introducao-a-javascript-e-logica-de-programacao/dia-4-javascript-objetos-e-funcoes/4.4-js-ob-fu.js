11b4291010d20963fcf691d0d78c9e0cdc5ceae7
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-19 19:34:18 -0300

    exercicios/4.4 - 6

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu.js
@@ -95,24 +95,24 @@ let info = {
   }
 
 
-
-// Usando o objeto abaixo, faça os exercícios a seguir:
+// 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
 
 let leitor = {
-  nome: 'Julia',
-  sobrenome: 'Pessoa',
+  nome: "Julia",
+  sobrenome: "Pessoa",
   idade: 21,
   livrosFavoritos: [
     {
-      titulo: 'O Pior Dia de Todos',
-      autor: 'Daniela Kopsch',
-      editora: 'Tordesilhas',
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
     },
   ],
 };
 
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
 
-// 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
 
 
 // 7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
