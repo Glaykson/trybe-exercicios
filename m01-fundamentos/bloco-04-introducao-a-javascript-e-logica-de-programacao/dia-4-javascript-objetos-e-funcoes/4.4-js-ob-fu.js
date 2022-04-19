550dcd6387632eb034753434be8a9d4c211e37cd
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-19 15:03:20 -0300

    exercicios/4.4 - 1

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu.js
@@ -4,19 +4,24 @@
 
 // Usando o objeto abaixo, faça os exercícios a seguir:
 
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-};
-
+//let info = {
+//  personagem: 'Margarida',
+// origem: 'Pato Donald',
+//  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//};
 
 
 // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
-
 // Bem-vinda, Margarida
 
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
 
+  console.log("Bem-vinda, " + info.personagem);
+  
 
 // 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
 
