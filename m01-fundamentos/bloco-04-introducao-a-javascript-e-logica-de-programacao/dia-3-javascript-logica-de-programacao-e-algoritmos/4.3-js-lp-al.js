0f03dc62e161eceb173c18ffa3c1494c18869f9e
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-19 11:36:11 -0300

    exercicios/4.3 - 3

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/4.3-js-lp-al.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/4.3-js-lp-al.js
@@ -29,7 +29,23 @@ console.log(inverter);
 // Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
 let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
 
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index]; 
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < menorPalavra.length) {
+    menorPalavra = array[index];
+  }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);
 
 
 // 4- Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50. 
