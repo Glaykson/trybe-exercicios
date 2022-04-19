d21f474abb032fd6d424a69a76e6816571ef7317
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-19 15:23:48 -0300

    exercicios/4.4 - 2

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu.js
@@ -23,26 +23,35 @@ let info = {
   console.log("Bem-vinda, " + info.personagem);
   
 
-// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+// 2 - Insira no objeto uma nova chaves com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
 
-{
+let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   recorrente: 'Sim'
 }
+info.recorrente = 'Sim';
 
-
-// 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
-
-// personagem
-// origem
-// nota
-// recorrente
+console.log(info);
 
 
 
-// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// 3 - Faça um for/in que mostre todas as chaves do objeto.
+
+
+
+
+// 4 - Faça um novo for/in , mas agora mostre todos os chaves das chaves do objeto. Valor esperado no console:
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  }
+
+
 
 // Margarida
 // Pato Donald
@@ -51,7 +60,7 @@ let info = {
 
 
 
-// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes chaves: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os chaves de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
 
 // Margarida e Tio Patinhas
 // Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
