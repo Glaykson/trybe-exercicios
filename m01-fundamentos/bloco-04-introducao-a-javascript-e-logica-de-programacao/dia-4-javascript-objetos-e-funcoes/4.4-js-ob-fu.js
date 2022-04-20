0e4400671fa729d413c1240314b6e8a566159c27
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-20 10:52:14 -0300

    exercicios/4.4 - 7 e 8

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu.js
@@ -114,17 +114,52 @@ console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + ' se
 
 
 
-
 // 7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
 
-{
-  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-  autor: 'JK Rowling',
-  editora: 'Rocco',
-}
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
+ };
+
+  leitor.livrosFavoritos.push(
+  {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco',
+  },
+)
+
 
 
 // 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
 
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
+ };
 
+  leitor.livrosFavoritos.push(
+  {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco',
+  },
+);
 
+console.log('"' + leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos" + '"');
