93d6f38d5cafad6cfe70df8cac7063cc0277cbe3
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-21 17:23:06 -0300

    exercicios/4.4b - 05

--- m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu-2.js
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu-2.js
@@ -64,7 +64,35 @@ function maisCaracteres(nomes) {
 console.log(maisCaracteres(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 
 
+
 // 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete. 
+
+function maisRepetido(array) {
+    let num = {};
+  
+    for (let index = 0; index < array.length; index += 1) {
+      let valor = array[index];
+      if (num[valor] === undefined) {
+        num[valor] = 1;
+      } else {
+        num[valor] = num[valor] + 1;
+      }
+    }
+  
+    let contRepetido = 0;
+    let contNumero = 0;
+  
+    for (let prop in num) {
+      if (contRepetido < num[prop]) {
+        contRepetido = num[prop];
+        contNumero = prop;
+      }
+    }
+  
+    return contNumero;
+}
+  
+console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3]));
 
 
 
