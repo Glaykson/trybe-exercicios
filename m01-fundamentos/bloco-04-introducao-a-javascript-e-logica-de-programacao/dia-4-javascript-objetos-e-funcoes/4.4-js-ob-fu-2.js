b84760272f0f9f3bd1224f1c3607a50de2cfb5a7
Glaykson Rodrigues <glaykson@gmail.com>
2022-04-21 14:47:23 -0300

    exercicios/4.4b - 01

---
+++ m01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/4.4-js-ob-fu-2.js
@@ -0,0 +1,55 @@
+//      4.2 - JavaScript - Objetos e funções
+
+// Parte II - Funções
+
+
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
+
+function palindromo = 'ovo'
+
+for (index = 0; index < ovo.length; index += 1) {
+  if (ovo[index])
+}
+
+
+function palindromeOuNao(ovo){
+    for(index in ovo) {
+      if(ovo[index] != ovo[(ovo.length - 1) - index]){
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  console.log(palindromeOuNao('ovo'));
+
+
+
+
+
+
+
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor. 
+
+
+
+
+
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor. 
+
+
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete. 
+
+
+
+
+// 6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N. 
+
+
+
+
+
+// 7 - Crie uma função que receba uma ovo ovo e outra ovo ending . Verifique se a ovo ending é o final da ovo ovo . Considere que a ovo ending sempre será menor que a ovo ovo . 
+
+
