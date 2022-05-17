aebc5e2bb1e989ffa94c89d173a35e1a072ca563
Glaykson Rodrigues <glaykson@gmail.com>
2022-05-17 15:56:19 -0300

    exercicios/8.1 - 3

--- m01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ m01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,7 +1,7 @@
 
 // ................... Exercício 1 ...................
 
-const gerarFuncionario = (nomeCompleto) => {
+/* const gerarFuncionario = (nomeCompleto) => {
   const email = nomeCompleto.toLowerCase().split(' ').join('_');
   return { nomeCompleto, email: `${email}@trybe.com` };
 }
@@ -25,4 +25,28 @@ const numSorteado = (meuNum, verifica) => {
   return verifica(meuNum, num) ? 'Parabéns você ganhou!' : 'Tente novamente';
 };
 console.log(numSorteado(3, verificaNum));
+ */
+// ................... Exercício 3 ...................
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const comparaRespostas = (respostaCerta, respostaDoAluno) => {
+  if (respostaCerta === respostaDoAluno) {
+    return 1;
+  } if (respostaDoAluno === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+};
+
+const contaPontos = (respostasCorretas, respostasDoEstudante, compara) => {
+  let contador = 0;
+  for (let i = 0; i < respostasCorretas.length; i += 1) {
+    const retornaComparacao = compara(respostasCorretas[i], respostasDoEstudante[i]);
+    contador += retornaComparacao;
+  }
+  return `Resultado final: ${contador} pontos`;
+};
+
+console.log(contaPontos(RIGHT_ANSWERS, STUDENT_ANSWERS, comparaRespostas));
 
