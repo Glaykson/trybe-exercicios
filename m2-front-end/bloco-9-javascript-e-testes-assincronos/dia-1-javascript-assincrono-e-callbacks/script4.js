bdaf7ba9c06fcc7c5b13e09b481af6e8f5fa0c81
Glaykson Rodrigues <glaykson@gmail.com>
2022-06-02 13:26:57 -0300

    exercicios/9.1 - 4

--- m2-front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script4.js
+++ m2-front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script4.js
@@ -3,10 +3,28 @@
 const messageDelay = () => Math.floor(Math.random() * 5000);
 
 const getMarsTemperature = () => {
-  const maxTemperature = 58;
-  return Math.floor(Math.random() * maxTemperature);
+    const maxTemperature = 58;
+    return Math.floor(Math.random() * maxTemperature);
 };
 
-// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+  setTimeout(() => console.log(`A temperatura de Marte é: ${getMarsTemperature()} graus Celsius`), messageDelay()); 
+}
 
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+// .......................................................
+
+// const messageDelay = () => Math.floor(Math.random() * 5000);
+// const maxTemperature = 58;
+// const getMarsTemperature = () => Math.floor(Math.random() * maxTemperature);
+
+// setTimeout(() => console.log(`A temperatura de Marte é: ${getMarsTemperature()} graus Celsius`), messageDelay()); 
+
+// getMarsTemperature();
+
+// Tradução:
+// messageDelay: Atraso da mensagem
+// getMarsTemperature: obter a temperatura de Marte
+// maxTemperature: Temperatura máxima
+// sendMarsTemperature: enviar temperatura de Marte
