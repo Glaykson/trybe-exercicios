76f68edbd138d5748364a5fc8e616b180b33910c
Glaykson Rodrigues <glaykson@gmail.com>
2022-06-04 18:15:19 -0300

    exercicios/9.1 - 10

---
+++ m2-front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script10.test.js
@@ -0,0 +1,13 @@
+// 10 - Para este exercício, tente adivinhar a saída dos console.log dos testes abaixo sem executá-los, e veja se compreendeu bem o funcionamento do beforeEach e do afterEach.
+
+beforeEach(() => console.log('1 - beforeEach')); // antes de cada teste
+afterEach(() => console.log('1 - afterEach')); // depois de cada teste
+
+test('', () => console.log('1 - test')); // o teste
+
+describe('Scoped / Nested block', () => {
+  beforeEach(() => console.log('2 - beforeEach')); // antes de cada teste no escopo do describe.
+  afterEach(() => console.log('2 - afterEach')); // depois de cada teste no escopo do describe.
+
+  test('', () => console.log('2 - test')); // o teste no escopo do describe.
+});
